5d5ec3ebb7278801d45ffcf5d4ac213d8aca7e9b
Iceman-cologne <aysunsunay@imac.fritz.box>
2015-01-17 12:15:35 +0100

    Initial checkin of aysun

---
+++ uni/src/main/java/com/youapp/spring/uni/App.java
@@ -0,0 +1,13 @@
+package com.youapp.spring.uni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Aysun!" );
+    }
+}
