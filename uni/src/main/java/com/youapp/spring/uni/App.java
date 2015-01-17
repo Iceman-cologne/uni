f94258e300900be722f0de723d92351a854554a4
Iceman-cologne <aysunsunay@imac.fritz.box>
2015-01-17 12:25:16 +0100

    new message

--- uni/src/main/java/com/youapp/spring/uni/App.java
+++ uni/src/main/java/com/youapp/spring/uni/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World Aysun!" );
+        System.out.println("Hallo was machst du?");
     }
 }
